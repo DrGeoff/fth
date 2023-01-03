0d502b759c2dc305134ea203ee59bc1fcf0ad439
DrGeoff <drgeoffathome@gmail.com>
2023-01-03 21:24:57 +1000

    Removed comment

--- dictionary.hpp
+++ dictionary.hpp
@@ -9,7 +9,6 @@
 // The purpose of some functions is to manipulate the dictionary so that must be available to all functions
 struct Dictionary;
 using FthFunc = void(Stack& stack, ReturnStack& returnStack, Dictionary& dictionary);
-//typedef void (*FthFunc)(Stack& stack, ReturnStack& returnStack, Dictionary& dictionary);
 
 struct Dictionary : public std::unordered_map<std::string, std::function<FthFunc>> 
 {
