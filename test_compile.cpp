ed305d8d0b1fb854743dde87cad4d17a5583f385
DrGeoff <drgeoffathome@gmail.com>
2023-02-06 10:35:36 +1000

    Added a test of compile mode

---
+++ test_compile.cpp
@@ -0,0 +1,21 @@
+//#PKG-CONFIG=gtest_main
+
+#include "test_utils.hpp"
+#include <gtest/gtest.h>
+
+
+namespace
+{
+
+TEST(compile_mode, fortytwo)
+{
+    const std::string program = R"(: fortytwo 42
+fortytwo
+.)";
+    const std::string output = fth::harness(program);
+    EXPECT_EQ(output,"42");
+}
+
+}  // namespace
+
+
